f86c9be5276205e88be28aeb9de021139004a876
Eduardo Melzer <eduardoalmelzer@gmail.com>
2015-06-20 17:49:33 -0300

    Ajustes nas telas de cadastro de usuário

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -17,7 +17,7 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
             //url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
             url = "jdbc:h2:file:/opt/h2/db/shurima;MVCC=TRUE;LOCK_TIMEOUT=10000"
         }
