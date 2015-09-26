6a90301e1fe3cacb8686d35d9a11e80c78e3145b
Eduardo Melzer <eduardoalmelzer@gmail.com>
2015-09-26 15:06:56 -0300

    Marshaller

--- src/groovy/com/shurima/CustomMarshallerRegistrar.groovy
+++ src/groovy/com/shurima/CustomMarshallerRegistrar.groovy
@@ -6,95 +6,94 @@ class CustomMarshallerRegistrar {
 
     void registerMarshallers() {
 
-		JSON.registerObjectMarshaller(com.shurima.Usuario) {
-			def map = [:]
-			map['id'] = it?.id
-			map['username'] = it?.username
-			map['password'] = it?.password
-      map['tipoUsuario'] = it?.tipoUsuario
-	    	map['toText'] = it?.toString()
-			return map
-		}
+        JSON.registerObjectMarshaller(com.shurima.Usuario) {
+            def map = [:]
+            map['id'] = it?.id
+            map['username'] = it?.username
+            map['password'] = it?.password
+            map['tipoUsuario'] = it?.tipoUsuario
+            map['toText'] = it?.toString()
+            return map
+        }
 
-    JSON.registerObjectMarshaller(com.shurima.TipoUsuario) {
-			def map = [:]
-			map['id'] = it?.id
-			map['descricao'] = it?.descricao
-	    	map['toText'] = it?.toString()
-			return map
-		}
+        JSON.registerObjectMarshaller(com.shurima.TipoUsuario) {
+            def map = [:]
+            map['id'] = it?.id
+            map['descricao'] = it?.descricao
+            map['toText'] = it?.toString()
+            return map
+        }
 
 
 
-		JSON.registerObjectMarshaller(com.shurima.Item) {
-			def map = [:]
-			map['id'] = it?.id
-			map['nome'] = it?.nome
-			map['quantidade'] = it?.quantidade
-			map['valor1'] = it?.valor1
-			map['valor2'] = it?.valor2
-			map['valor3'] = it?.valor3
-			map['unidade'] = it?.unidade
-        map['toText'] = it?.toString()
-    return map
+        JSON.registerObjectMarshaller(com.shurima.Item) {
+            def map = [:]
+            map['id'] = it?.id
+            map['nome'] = it?.nome
+            map['quantidade'] = it?.quantidade
+            map['valor1'] = it?.valor1
+            map['valor2'] = it?.valor2
+            map['valor3'] = it?.valor3
+            map['unidade'] = it?.unidade
+            map['toText'] = it?.toString()
+            return map
+        }
+
+
+
+        JSON.registerObjectMarshaller(com.shurima.Transacao) {
+            def map = [:]
+            map['id'] = it?.id
+            map['data'] = it?.data
+            map['valor'] = it?.valor
+            map['empresa'] = it?.empresa
+            map['tipo'] = it?.tipo
+            map['numeroTitulo']=it?.numeroTitulo
+            map['observacao']=it?.observacao
+            map['toText'] = it?.toString()
+            return map
+        }
+
+        JSON.registerObjectMarshaller(com.shurima.Empresa) {
+            def map = [:]
+            map['id'] = it?.id
+            map['nome'] = it?.nome
+            map['cnpj'] = it?.cnpj
+            map['inscricaoEstadual'] = it?.inscricaoEstadual
+            map['endereco'] = it?.endereco
+            map['numero'] = it?.numero
+            map['complemento'] = it?.complemento
+            map['cidade'] = it?.cidade
+            map['bairro'] = it?.bairro
+            map['UF'] = it?.UF
+            map['CEP'] = it?.CEP
+            map['telefone1'] = it?.telefone1
+            map['telefone2'] = it?.telefone2
+            map['email'] = it?.email
+            map['isCliente'] = it?.isCliente
+            map['isFornecedor'] = it?.isFornecedor
+            map['toText'] = it?.toString()
+            return map
+        }
+
+        JSON.registerObjectMarshaller(com.shurima.Cliente) {
+            def map = [:]
+            map['id'] = it?.id
+            map['nome'] = it?.nome
+            map['cnpj'] = it?.cnpj
+            map['inscricaoEstadual'] = it?.inscricaoEstadual
+            map['endereco'] = it?.endereco
+            map['numero'] = it?.numero
+            map['complemento'] = it?.complemento
+            map['cidade'] = it?.cidade
+            map['bairro'] = it?.bairro
+            map['UF'] = it?.UF
+            map['CEP'] = it?.CEP
+            map['telefone1'] = it?.telefone1
+            map['telefone2'] = it?.telefone2
+            map['email'] = it?.email
+            map['toText'] = it?.toString()
+            return map
+        }
     }
-
-
-
-		JSON.registerObjectMarshaller(com.shurima.Transacao) {
-			def map = [:]
-			map['id'] = it?.id
-			map['data'] = it?.data
-			map['valor'] = it?.valor
-			map['empresa'] = it?.empresa
-			map['tipo'] = it?.tipo
-      map['numeroTitulo']=it?.numeroTitulo
-      map['observacao']=it?.observacao
-	     map['toText'] = it?.toString()
-			return map
-		}
-
-		JSON.registerObjectMarshaller(com.shurima.Empresa) {
-			def map = [:]
-			map['id'] = it?.id
-			map['nome'] = it?.nome
-			map['cnpj'] = it?.cnpj
-			map['inscricaoEstadual'] = it?.inscricaoEstadual
-			map['endereco'] = it?.endereco
-			map['numero'] = it?.numero
-			map['complemento'] = it?.complemento
-			map['cidade'] = it?.cidade
-			map['bairro'] = it?.bairro
-			map['UF'] = it?.UF
-			map['CEP'] = it?.CEP
-			map['telefone1'] = it?.telefone1
-			map['telefone2'] = it?.telefone2
-			map['email'] = it?.email
-			map['isCliente'] = it?.isCliente
-			map['isFornecedor'] = it?.isFornecedor
-	    	map['toText'] = it?.toString()
-			return map
-		}
-
-		JSON.registerObjectMarshaller(com.shurima.Cliente) {
-			def map = [:]
-			map['id'] = it?.id
-			map['nome'] = it?.nome
-			map['cnpj'] = it?.cnpj
-			map['inscricaoEstadual'] = it?.inscricaoEstadual
-			map['endereco'] = it?.endereco
-			map['numero'] = it?.numero
-			map['complemento'] = it?.complemento
-			map['cidade'] = it?.cidade
-			map['bairro'] = it?.bairro
-			map['UF'] = it?.UF
-			map['CEP'] = it?.CEP
-			map['telefone1'] = it?.telefone1
-			map['telefone2'] = it?.telefone2
-			map['email'] = it?.email
-	    	map['toText'] = it?.toString()
-			return map
-		}
-	}
-
 }
